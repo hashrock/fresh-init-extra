413a903a0c8fda4e678361c5dc4d1faafebca6c7
hashrock <hashrock@users.noreply.github.com>
2023-06-10 00:31:01 +0900

    add warning

--- juicer.ts
+++ juicer.ts
@@ -1,8 +1,18 @@
 import generate from "./lib/generate.ts";
 
-const title = `\x1b[33m juicer\x1b[0m - A scaffolding tool for Fresh projects `;
+const title = `\x1b[33mjuicer\x1b[0m - A scaffolding tool for Fresh projects`;
 
+console.log("");
 console.log(title);
-console.log("\n");
+console.log("");
+
+try {
+  Deno.statSync("fresh.gen.ts");
+} catch (_) {
+  console.log(
+    "\x1b[31mWARNING: Please run this command from the root of your fresh project\x1b[0m",
+  );
+  console.log("");
+}
 
 await generate();
